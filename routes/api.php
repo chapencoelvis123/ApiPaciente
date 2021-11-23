5078062bf0603f3f13a9b966bb13079cac373786
chapencoelvis123 <chapencoelvis123@gmail.com>
2021-11-23 18:14:20 -0400

    implementacion de api CRUD de pacientes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\PacienteController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('pacientes', [PacienteController::class, 'index']);
+Route::post('pacientes', [PacienteController::class, 'store']);
+Route::get('pacientes/{paciente}', [PacienteController::class, 'show']);
+Route::put('pacientes/{paciente}', [PacienteController::class, 'update']);
+Route::delete('pacientes/{paciente}', [PacienteController::class, 'destroy']);
